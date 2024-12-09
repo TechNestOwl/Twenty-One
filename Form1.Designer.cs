b1f02e405386d17e14110512b684f68f1d141d85
Guilherme Guimaraes <technestowl@gmail.com>
2024-12-09 11:20:56 -0500

    Nearly done with build. Need to work on svg resources.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,25 +32,33 @@
             rollOneDie = new Button();
             hitCount = new Label();
             label2 = new Label();
+            ExitBtn = new Button();
+            PlayAgainBtn = new Button();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
             // 
             // rollTwoDice
             // 
-            rollTwoDice.Location = new Point(477, 79);
+            rollTwoDice.Location = new Point(281, 78);
             rollTwoDice.Name = "rollTwoDice";
             rollTwoDice.Size = new Size(83, 25);
             rollTwoDice.TabIndex = 0;
             rollTwoDice.Text = "Roll 2 Dice";
             rollTwoDice.UseVisualStyleBackColor = true;
+            rollTwoDice.Click += rollTwoDice_Click_1;
             // 
             // rollOneDie
             // 
-            rollOneDie.Location = new Point(281, 79);
+            rollOneDie.Location = new Point(477, 78);
             rollOneDie.Name = "rollOneDie";
             rollOneDie.Size = new Size(83, 25);
             rollOneDie.TabIndex = 1;
             rollOneDie.Text = "Roll One Die";
             rollOneDie.UseVisualStyleBackColor = true;
+            rollOneDie.Click += rollOneDie_Click_1;
             // 
             // hitCount
             // 
@@ -69,17 +77,59 @@
             label2.Size = new Size(0, 17);
             label2.TabIndex = 3;
             // 
+            // ExitBtn
+            // 
+            ExitBtn.Location = new Point(477, 361);
+            ExitBtn.Name = "ExitBtn";
+            ExitBtn.Size = new Size(83, 25);
+            ExitBtn.TabIndex = 6;
+            ExitBtn.Text = "Exit";
+            ExitBtn.UseVisualStyleBackColor = true;
+            ExitBtn.Click += ExitBtn_Click;
+            // 
+            // PlayAgainBtn
+            // 
+            PlayAgainBtn.Location = new Point(281, 361);
+            PlayAgainBtn.Name = "PlayAgainBtn";
+            PlayAgainBtn.Size = new Size(83, 25);
+            PlayAgainBtn.TabIndex = 7;
+            PlayAgainBtn.Text = "Play Again";
+            PlayAgainBtn.UseVisualStyleBackColor = true;
+            PlayAgainBtn.Click += PlayAgainBtn_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(477, 165);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(110, 55);
+            pictureBox1.TabIndex = 8;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(281, 165);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(110, 55);
+            pictureBox2.TabIndex = 9;
+            pictureBox2.TabStop = false;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 17F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(880, 615);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(PlayAgainBtn);
+            Controls.Add(ExitBtn);
             Controls.Add(label2);
             Controls.Add(hitCount);
             Controls.Add(rollOneDie);
             Controls.Add(rollTwoDice);
             Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -90,5 +140,9 @@
         private Button rollOneDie;
         private Label hitCount;
         private Label label2;
+        private Button ExitBtn;
+        private Button PlayAgainBtn;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,122 @@
+//Gill Guimaraes
+// Mini-Project #2
+
+using Microsoft.VisualBasic.ApplicationServices;
+using System.Security.Policy;
+using Twenty_One.Properties;
+
 namespace Twenty_One
 {
     public partial class Form1 : Form
     {
+        private int totalScore = 0;
+        private Random random = new Random();
+
         public Form1()
         {
             InitializeComponent();
+            ResetGame(); // Initializin the game
+            PlayAgainBtn.Visible = false; // Hiding Play Again button initially
         }
+
+        private void rollOneDie_Click_1(object sender, EventArgs e)
+        {
+            int roll = random.Next(1, 7);
+            totalScore += roll;
+
+            // Display the corresponding dice image
+            pictureBox1.Image = GetDiceImage(roll);
+            pictureBox2.Image = null; // not displaying the second dice
+
+            UpdateGameState();
+        }
+
+        private void rollTwoDice_Click_1(object sender, EventArgs e)
+        {
+            int roll1 = random.Next(1, 7);
+            int roll2 = random.Next(1, 7);
+            totalScore += (roll1 + roll2);
+
+            // Show both dice images
+            pictureBox1.Image = GetDiceImage(roll1);
+            pictureBox2.Image = GetDiceImage(roll2);
+
+            UpdateGameState();
+        }
+
+        private Image GetDiceImage(int diceValue)
+        {
+            switch (diceValue)
+            {
+                case 1: return Properties.Resources.dice1; 
+                case 2: return Properties.Resources.dice2;
+                case 3: return Properties.Resources.dice3;
+                case 4: return Properties.Resources.dice4;
+                case 5: return Properties.Resources.dice5;
+                case 6: return Properties.Resources.dice6;
+                default: return null;
+            }
+        }
+        private void hitCount_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void UpdateGameState()
+        {
+            // update hitCount label
+            hitCount.Text = totalScore.ToString();
+
+            // Check my win/lose conditions
+            if (totalScore == 21)
+            {
+                DialogResult result = MessageBox.Show(
+                    "Congratulations! You hit 21!\nWould you like to play again?",
+                    "Winner!",
+                    MessageBoxButtons.YesNo);
+
+                HandleGameEnd(result);
+            }
+            else if (totalScore > 21)
+            {
+                DialogResult result = MessageBox.Show(
+                    $"Game Over! You went over 21 with {totalScore}!\nWould you like to play again?",
+                    "Game Over",
+                    MessageBoxButtons.YesNo);
+
+                HandleGameEnd(result);
+            }
+        }
+
+        private void HandleGameEnd(DialogResult result)
+        {
+            if (result == DialogResult.Yes)
+            {
+                ResetGame();
+            }
+            else
+            {
+                this.Close();
+            }
+        }
+
+        private void ResetGame()
+        {
+            totalScore = 0;
+            hitCount.Text = "0";
+            pictureBox1.Image = null;
+            pictureBox2.Image = null;
+        }
+
+        private void PlayAgainBtn_Click(object sender, EventArgs e)
+        {
+            ResetGame();
+        }
+
+        private void ExitBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+      
     }
 }
